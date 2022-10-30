e57f2f8b5636beb38fb09df9e1fed0d0275052ff
Seth DImmer <sethdimmer1@gmail.com>
2022-10-30 06:34:13 -0400

    initial commit

--- index.js
+++ index.js
@@ -4,14 +4,20 @@
 
 function contact(event) {
     event.preventDefault(); //prevents refreshing of email page
-    emailjs
-      .sendForm(
-        'service_pker4n9',
-        'template_og3t8jr',
-        event.target,
-        'hXfeDCAUBiwaZ6K3T'
-      ).then(() => {
-        console.log('this worked')
-      })
+    // emailjs
+    //   .sendForm(
+    //     'service_pker4n9',
+    //     'template_og3t8jr',
+    //     event.target,
+    //     'hXfeDCAUBiwaZ6K3T'
+    //   ).then(() => {
+    //     console.log('this worked')
+    //   })
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    
+    setTimeout(() => {
+        console.log('it worked')
+    },200);
 
 }
