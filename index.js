1de3651472d7bf5bfb9a99caa781b455b5e86359
Seth DImmer <sethdimmer1@gmail.com>
2022-10-30 14:22:50 -0400

    initial commit

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 
 function contact(event) {
     event.preventDefault(); //prevents refreshing of email page
-    const loading = document.querySelector('.modal__overlay--loading')
-    const success = document.querySelector('.modal__overlay--success')
-    loading.classList.remove("modal__overlay--visible")
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += "modal__overlay--visible";
     emailjs
       .sendForm(
         'service_pker4n9',
@@ -14,21 +14,23 @@ function contact(event) {
         event.target,
         'hXfeDCAUBiwaZ6K3T'
       ).then(() => {
-        // throw new Error("error");
         loading.classList.remove("modal__overlay--visible")
         success.classList += " modal__overlay--visible";
       }).catch(() => { //if the service is down or i run out of emails this is what happens
         loading.classList.remove("modal__overlay--visible")
         alert(
             "The email service is temporarily unavailable. Please contact me directly on sethdimmer1@gmail.com"
-        )
-
+        );
       })
+    }
 
-    loading.classList += " modal__overlay--visible"
-    setTimeout(() => {
 
-        console.log('it worked')
-    },1000);
-
+let isModalOpen = false;
+function toggleModal() {
+    if(isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open"
 }
