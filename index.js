c1416618190d58688c906e018b131a87ade9d7cd
Seth DImmer <sethdimmer1@gmail.com>
2022-10-30 12:27:48 -0400

    initial commit

--- index.js
+++ index.js
@@ -4,20 +4,31 @@
 
 function contact(event) {
     event.preventDefault(); //prevents refreshing of email page
-    // emailjs
-    //   .sendForm(
-    //     'service_pker4n9',
-    //     'template_og3t8jr',
-    //     event.target,
-    //     'hXfeDCAUBiwaZ6K3T'
-    //   ).then(() => {
-    //     console.log('this worked')
-    //   })
     const loading = document.querySelector('.modal__overlay--loading')
     const success = document.querySelector('.modal__overlay--success')
-    
+    loading.classList.remove("modal__overlay--visible")
+    emailjs
+      .sendForm(
+        'service_pker4n9',
+        'template_og3t8jr',
+        event.target,
+        'hXfeDCAUBiwaZ6K3T'
+      ).then(() => {
+        // throw new Error("error");
+        loading.classList.remove("modal__overlay--visible")
+        success.classList += " modal__overlay--visible";
+      }).catch(() => { //if the service is down or i run out of emails this is what happens
+        loading.classList.remove("modal__overlay--visible")
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly on sethdimmer1@gmail.com"
+        )
+
+      })
+
+    loading.classList += " modal__overlay--visible"
     setTimeout(() => {
+
         console.log('it worked')
-    },200);
+    },1000);
 
 }
