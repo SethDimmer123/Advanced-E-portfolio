364c8304f97b610e7987d639a1198b0af8f55070
Seth DImmer <sethdimmer1@gmail.com>
2022-11-30 17:34:33 -0500

    initial commit

--- index.js
+++ index.js
@@ -4,20 +4,30 @@
 
 function contact(event) {
     event.preventDefault();
-    // emailjs
-    //     .sendForm(
-    //         'service_sj0b8fa',
-    //         'template_og3t8jr',
-    //         event.target,
-    //         'hXfeDCAUBiwaZ6K3T'
-    //     ).then(() => {
-    //         console.log('this worked1')
-    //     })
     const loading = document.querySelector('.modal__overlay--loading')
     const success = document.querySelector('.modal__overlay--success')
-    
+
+    emailjs
+        .sendForm(
+            'service_sj0b8fa',
+            'template_og3t8jr',
+            event.target,
+            'hXfeDCAUBiwaZ6K3T'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible")
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible")
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly on sethdimmer1@gmail.com"
+            )
+        })
+
+
+    loading.classList += " modal__overlay--visible" 
     setTimeout(() => {
+
         console.log('it worked 1')
-    }, 500);
+    }, 1000);
 
 }
